4935de77432f745f200a4868fbde20a769bc5e91
Deekshas003 <163335192+Deekshas003@users.noreply.github.com>
2025-05-18 13:41:52 +0530

    data_analysis.py

--- data_analysis.py
+++ data_analysis.py
@@ -1,5 +1,13 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 print("Welcome to our data analysis project")
+
+# lead the tips dataset from seaborn
+tips= sns.load_dataset("tips")
+
+# printing few row of dataset
+print(tips.head())
+  
